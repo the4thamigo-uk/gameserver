5d1bd6e53b98a53e96db3cd3fc02702b74636157
Andrew Smith <andrew.smith@miracl.com>
2018-10-21 05:59:19 +0100

    Fix linter issues

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -5,18 +5,21 @@ import (
 	"github.com/the4thamigo_uk/gameserver/pkg/domain"
 )
 
+// Config defines the main settings of the game server
 type Config struct {
 	Address string
 	Hangman HangmanConfig
 	// ... other games here ...
 }
 
+// HangmanConfig defines the main settings for the Hangman game
 type HangmanConfig struct {
 	Words []string
 	Turns int
 	dict  *domain.Dictionary
 }
 
+// LoadConfig loads a config from a local file or url
 func LoadConfig(paths ...string) (*Config, error) {
 	c, err := conflate.FromFiles(paths...)
 	if err != nil {
@@ -27,14 +30,14 @@ func LoadConfig(paths ...string) (*Config, error) {
 	if err != nil {
 		return nil, err
 	}
-	err = cfg.Hangman.Init()
+	err = cfg.Hangman.init()
 	if err != nil {
 		return nil, err
 	}
 	return &cfg, nil
 }
 
-func (cfg *HangmanConfig) Init() error {
+func (cfg *HangmanConfig) init() error {
 	cfg.dict = domain.NewDictionary(cfg.Words)
 	return nil
 }
